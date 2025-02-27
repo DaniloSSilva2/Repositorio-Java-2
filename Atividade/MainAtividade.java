6b17d8b86b7e8775b4eb4602b8f0dcb0e8e5188d
DaniloSSilva2 <netunoss01234@gmail.com>
2025-02-27 00:30:26 +0000

    Simple

--- Atividade/MainAtividade.java
+++ Atividade/MainAtividade.java
@@ -7,10 +7,24 @@ Processador process1 = new Processador("Intel", "It720", 880);
 
 Memoria memoria1 = new Memoria("Nvidia", "Nvidia44s", 770);
 
+PlacaMae placamae1 = new PlacaMae("Nvidia", "tsx466", "LGA1150");
+
+DispositivoArmazenamento dpaz1 = new DispositivoArmazenamento("Intel", "ZGP54", 564, "HDD");
+
+
+
+
+
+
+
 System.out.println();
 System.out.println(process1.toString());
 System.out.println();
 System.out.println(memoria1.toString());
+System.out.println();
+System.out.println(placamae1.toString());
+System.out.println();
+System.out.println(dpaz1.toString());
 
 
 
